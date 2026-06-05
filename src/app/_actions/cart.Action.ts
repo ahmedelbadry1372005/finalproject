641e779fdf4f2188c5f8d1252545f86aac3b548c
ahmedelbadry1372005 <ahmedelbadry1372005@gmail.com>
2026-06-05 20:28:01 +0200

    fix updateProductCart token type

--- src/app/_actions/cart.Action.ts
+++ src/app/_actions/cart.Action.ts
@@ -4,76 +4,71 @@ import { cartResType } from "@/types/cart.type";
 import getMyToken from "../utils/getmyToken";
 
 export async function addProductToCart(
-  id: string,
-  token: string
+id: string,
+token: string
 ): Promise<cartResType> {
-  const res = await fetch("https://ecommerce.routemisr.com/api/v2/cart", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      token: token,
-    },
-    body: JSON.stringify({ productId: id }),
-  });
+const res = await fetch("https://ecommerce.routemisr.com/api/v2/cart", {
+method: "POST",
+headers: {
+"Content-Type": "application/json",
+token: token,
+},
+body: JSON.stringify({ productId: id }),
+});
 
-  return res.json();
+return res.json();
 }
-
 
 export async function getUserCart(): Promise<cartResType> {
-  const token = await getMyToken();
+const token = await getMyToken();
 
-  const res = await fetch("https://ecommerce.routemisr.com/api/v2/cart", {
-    headers: {
-      token: token as string,
-    },
-  });
+const res = await fetch("https://ecommerce.routemisr.com/api/v2/cart", {
+headers: {
+token: token as string,
+},
+});
 
-  return res.json();
+return res.json();
 }
-
 
 export async function deleteItemFromCart(
-  productId: string
+productId: string
 ): Promise<any> {
-  const token = await getMyToken();
+const token = await getMyToken();
 
-  const res = await fetch(
-    `https://ecommerce.routemisr.com/api/v2/cart/${productId}`,
-    {
-      method: "DELETE",
-      headers: {
-        token: token as string,
-      },
-    }
-  );
+const res = await fetch(
+`https://ecommerce.routemisr.com/api/v2/cart/${productId}`,
+{
+method: "DELETE",
+headers: {
+token: token as string,
+},
+}
+);
 
-  return res.json();
+return res.json();
 }
 
-
 export async function updateProductCart(
-  id: string,
-  count: number,
-  token?: string
+id: string,
+count: number,
+token?: string | null
 ): Promise<cartResType> {
+if (!token) {
+token = await getMyToken();
+}
 
-  
-  if (!token) {
-    token = await getMyToken();
-  }
+const res = await fetch(
+`https://ecommerce.routemisr.com/api/v2/cart/${id}`,
+{
+method: "PUT",
+headers: {
+"Content-Type": "application/json",
+token: token as string,
+},
+body: JSON.stringify({ count }),
+}
+);
 
-  const res = await fetch(
-    `https://ecommerce.routemisr.com/api/v2/cart/${id}`,
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        token: token as string,
-      },
-      body: JSON.stringify({ count }),
-    }
-  );
-
-  return res.json();
+return res.json();
 }
